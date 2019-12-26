17b622f8f6de8493698017610a6ab0e41a57ab92
weiye <weiye@cetcxl.com>
2019-12-26 11:33:22 +0800

    add

---
+++ src/main/java/com/treeke/WebSocketService.java
@@ -0,0 +1,35 @@
+package com.treeke;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSocketService {
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    /**
+     * 广播
+     * 发给所有在线用户
+     *
+     * @param msg
+     */
+    public void sendMsg(WiselyResponse msg) {
+        template.convertAndSend(Constant.PRODUCERPATH, msg);
+    }
+
+    /**
+     * 发送给指定用户
+     * @param users
+     * @param msg
+     */
+    public void send2Users(List<String> users, WiselyResponse msg) {
+        users.forEach(userName -> {
+            template.convertAndSendToUser(userName, Constant.P2PPUSHPATH, msg);
+        });
+    }
+}
---
+++ src/main/java/com/treeke/WsController.java
@@ -0,0 +1,27 @@
+package com.treeke;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WsController {
+
+    @Resource
+    WebSocketService webSocketService;
+
+    @MessageMapping(Constant.FORETOSERVERPATH)//@MessageMapping和@RequestMapping功能类似，用于设置URL映射地址，浏览器向服务器发起请求，需要通过该地址。
+    @SendTo(Constant.PRODUCERPATH)//如果服务器接受到了消息，就会对订阅了@SendTo括号中的地址传送消息。
+    public WiselyResponse say(WiselyMessage message) throws Exception {
+        List<String> users = new ArrayList<>();
+        users.add("d892bf12bf7d11e793b69c5c8e6f60fb");//此处写死只是为了方便测试,此值需要对应页面中订阅个人消息的userId
+        webSocketService.send2Users(users, new WiselyResponse("admin hello"));
+
+        return new WiselyResponse("Welcome, " + message.getName() + "!");
+    }
+}
